d528d284762fa0708527618a18ac79a732072cea
pwtail <abvit89s@gmail.com>
2022-10-19 18:48:47 +0300

    views

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,11 +1,16 @@
+import random
+
 from django.shortcuts import render
+from greenhack import as_async
 
 from kitchen.models import Order, prepare_order
 from souslik import asgi
 
+from django.http import JsonResponse
 
 # Create your views here.
 
+@as_async
 def food_delivery(request):
     order: Order = prepare_order(request)
     order.save()
@@ -19,3 +24,8 @@ def food_delivery(request):
             kitchen_error(resp)
 
 
+@as_async
+def kitchen(request):
+    order_id = request.DATA['order_id']
+    mins = random.randrange(30, 60)
+    return JsonResponse({'order_id': order_id, 'mins': mins})
