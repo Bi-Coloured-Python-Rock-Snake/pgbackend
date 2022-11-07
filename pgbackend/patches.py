431ae0f30522681c4016d39b7e4e279c56f4bff9
pwtail <abvit89s@gmail.com>
2022-11-07 12:55:09 +0100

    Atomic

--- pgbackend/patches.py
+++ pgbackend/patches.py
@@ -1,10 +1,15 @@
+from django.db import connections
 from django.db.transaction import Atomic
 
+import pgbackend.base
 from pgbackend import atomic
 
 
-def __new__(cls, *args, **kwargs):
-    return atomic.Atomic(*args, **kwargs)
+def __new__(cls, using, *args, **kwargs):
+    db = connections[using]
+    if isinstance(db, pgbackend.base.DatabaseWrapper):
+        return atomic.Atomic(using, *args, **kwargs)
+    return Atomic.__new__(Atomic, using, *args, **kwargs)
 
 
 Atomic.__new__ = __new__
