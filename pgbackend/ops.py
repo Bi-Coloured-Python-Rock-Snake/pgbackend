00573ac5af9bde8da4b3d40e5cc49b8f41fd8188
pwtail <abvit89s@gmail.com>
2022-12-11 02:40:41 +0100

    ops

--- pgbackend/ops.py
+++ pgbackend/ops.py
@@ -3,3 +3,7 @@ from django.db.backends.postgresql.operations import DatabaseOperations
 
 class DatabaseOperations(DatabaseOperations):
     compiler_module = "pgbackend.compiler"
+
+    def compose_sql(self, sql, params):
+        raise NotImplementedError
+        return mogrify(sql, params, self.connection)
